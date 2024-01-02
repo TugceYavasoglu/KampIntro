79271cddcaf7347079061be986b23cc4600d2d6a
TugceYavasoglu <y.tugce19@gmail.com>
2024-01-02 19:18:16 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety-tip güvenliği
+            /*string kategoriEtiketi = "Kategori"; //değer tutucu, alias
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);*/
+
+
+            /*string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";*/
+
+            //array-dizi
+
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya başlangıç için temel kurs",
+                "Java"
+            };
+
+            for (int i = 0; i <kurslar.Length; i++) 
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("sayfa sonu- footer");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            
+
+
+
+
+        }
+    }
+}
